51d7bc89ab01789a5d063b874cd38c68e928c529
Ciprian Burduja <ciprianburduja2006@gmail.com>
2026-09-09 11:39:44 +0300

    Changing admin orders

--- backend/guitarshop/orders/admin.py
+++ backend/guitarshop/orders/admin.py
@@ -4,7 +4,7 @@ from .models import Order
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ["id", "user", "full_name", "country", "amount_total", "created_at"]
+    list_display = ["id", "user", "full_name", "address_line1","address_line2","city","postal_code","country", "amount_total", "created_at"]
     list_filter = ["country", "created_at"]
     search_fields = ["full_name", "stripe_session_id", "user__email"]
     readonly_fields = [f.name for f in Order._meta.fields]  # comenzile nu se editează manual
